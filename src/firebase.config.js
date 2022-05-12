b8074dee3ef99823ae1425e3689c765511178261
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-12 20:49:03 +0430

    project and config file for firebase added

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import {getFirestore} from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+    
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_881bmb3QMueYd8BUPl6vxSHeKkAuzgI",
+    authDomain: "house-marketplace-app-fa67e.firebaseapp.com",
+    projectId: "house-marketplace-app-fa67e",
+    storageBucket: "house-marketplace-app-fa67e.appspot.com",
+    messagingSenderId: "952964036513",
+    appId: "1:952964036513:web:aa163935bca26cd0063de0"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore
