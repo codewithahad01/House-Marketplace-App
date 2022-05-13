9ca32078c5f529584a84c4b78042510bad2b5dca
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-13 23:15:50 +0430

    components directory created for collecting components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useNavigate, useLocation} from 'react-router-dom'
+impor
+
+function Navbar() {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+export default Navbar
