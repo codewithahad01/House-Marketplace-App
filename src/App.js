78471a7ca3934d8763e3498886adf4b5f26aab50
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-13 23:14:13 +0430

    pages created and router set to every page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import ForgotPassword from './pages/ForgotPassword'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* Navbar */}
+    </>
   );
 }
 
