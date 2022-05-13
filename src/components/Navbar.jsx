a5ff9cf576d09601792ccbe87fc4acd4bfac14c9
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-13 23:56:28 +0430

    working with navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import {useNavigate, useLocation, Navigate} from 'react-router-dom'
+import {useNavigate, useLocation} from 'react-router-dom'
 import {ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
 import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
 import {ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
 
 
 function Navbar() {
-    const 
-    
+    // const navigate = useNavigate()
+    // const location = useLocation()
     return (
         <footer className='navbar'>
             <div className='navbarNav'>
                 <ul className='navbarListItems'>
-                    <li className='navbarListItem' onClick={() => Navigate (
-
-                    )}>
+                    <li className='navbarListItem' onClick={() => navigate}>
                         <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
                         <p>Explore</p>
                     </li>
