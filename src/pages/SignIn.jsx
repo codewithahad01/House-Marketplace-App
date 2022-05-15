db6210d4f2c2af3b6cab97daecf9220bb7b7973b
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-15 22:03:54 +0430

    working in sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,15 @@
+import {useState} from 'react'
+import {link, useNavigate} from 'react-router-dom'
+import {ReactComponent as ArrowRight} from '../assets/svg/keyboardArrowRightIcon.svg' 
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import React from 'react'
 
 function SignIn() {
+    const [showPassword, setPassword] = useState(false)
+    const [formData, setFormData] = useState({
+        email: ''
+    })
+
     return (
         <div>SignIn</div>
     )
