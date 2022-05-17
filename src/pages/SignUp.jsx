4f6cb77557f4f7d53847e3c33ed694eb663a769e
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-17 11:48:03 +0430

    signup fuctionality works fine

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {doc, setDoc, serverTimestamp} from 'firebase/firestore'
+import {db} from '../firebase.config'
 import {ReactComponent as ArrowRight} from '../assets/svg/keyboardArrowRightIcon.svg' 
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import React from 'react'
@@ -21,6 +24,27 @@ function SignUp() {
         }))
     }
 
+    const onSubmitForm = async (e) => {
+        e.preventDefault()
+        try{
+            const auth = getAuth()
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+            const user = userCredential.user
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            const formDataCopy = {...formData}
+            delete formDataCopy.password
+            formDataCopy.timestamp = serverTimestamp()
+            await setDoc(doc(db, 'users', user.uid), formDataCopy)
+            navigate('/')
+        }catch(error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <>
             <div className='pageContainer'>
@@ -30,7 +54,7 @@ function SignUp() {
                     </p>
                 </header>
 
-                    <form>
+                    <form onSubmit={onSubmitForm}>
                         <input type='text' className='nameInput' placeholder='Name' id='name' value={name} onChange={onChange} />
                         <input type='email' className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
 
