784911a379f1538a470f58118277bad8fff7f212
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-18 15:38:40 +0430

    showing slider for explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 function Explore() {
     return (
-        <div><h1>Explore</h1></div>
+        <div className='explore'>
+            <header>
+                <p className='pageHeader'>Explore</p>
+            </header>
+
+            <main>
+                {/* Slider */}
+
+                <p className='exploreCategoryHeading'>rentCategories</p>
+                <div className='exploreCategories'>
+                    <Link to='/category/rent'>
+                        <img className='exploreCategoryImg' src={rentCategoryImage} alt='rentCategory'/>
+                        <p className='exploreCategoryName'>Places for rent</p>
+                    </Link>
+
+                    <Link to='/category/sell'>
+                        <img className='exploreCategoryImg' src={sellCategoryImage} alt='sellCategory'/>
+                        <p className='exploreCategoryName'>Places for sale</p>
+                    </Link>
+                </div>
+            </main>
+        </div>
     )
 }
 
